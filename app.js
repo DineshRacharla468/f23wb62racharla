8b5fa9d02d61b313d863a4ad608f9e40d288b149
Dinesh Racharla <96189870+DineshRacharla468@users.noreply.github.com>
2023-11-01 19:00:30 -0500

    created choose and added imgs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dessertrouter = require('./routes/desserts');
 var boardrouter = require('./routes/board');
+var chooserouter = require('./routes/choose');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/desserts', dessertrouter);
 app.use('/board', boardrouter);
+app.use('/choose', chooserouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
